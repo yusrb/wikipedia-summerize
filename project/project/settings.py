9f6bc3bf021ae4e0cfe6d71e64131b5bfefa4563
yusrb <byrn.uiy@gmail.com>
2025-07-31 00:46:26 +0700

    Add procfile and gunicorn

--- project/project/settings.py
+++ project/project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-=15^f4mpsw!@rxj$t2&l^*6)tz13eu96s^)q_yg70f(*$r(_!#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -106,6 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
